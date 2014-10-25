20742a27b08dfcd30346aca9427492024d49719d
jin-yc10 <ccjinyc@gmail.com>
2014-10-25 15:09:38 +0800

    add empty validator

---
+++ DQCupJinYC/src/dqcup/repair/validators/AgeValidator.java
@@ -0,0 +1,23 @@
+package dqcup.repair.validators;
+
+public class AgeValidator implements Validator {
+
+	@Override
+	public int getIndex() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getColName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean test(String value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
