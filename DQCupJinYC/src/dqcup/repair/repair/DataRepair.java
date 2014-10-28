7e18d6aeec5a01350314be8f752f938b52af75c1
THSS-YuHeng <yuheng1002@gmail.com>
2014-10-28 11:15:49 +0800

    " "填充错误数据

--- DQCupJinYC/src/dqcup/repair/repair/DataRepair.java
+++ DQCupJinYC/src/dqcup/repair/repair/DataRepair.java
@@ -30,7 +30,7 @@ public class DataRepair {
 	private LinkedList<Tuple> tuples;					//原始数据链表存放
 	private HashSet<RepairedCell> result;				//修复结果
 	
-	public HashMap<String, LinkedList<Tuple>> correctTable;			//单行数据处理结果
+	public HashMap<String, LinkedList<Tuple>> correctTable;			//单行数据处理结果      ***使用“ ”作为错误的数据填充***
 	public HashMap<String, ErrorData> errorTable;			//单行数据错误
 
 	public ArrayList<Validator> validators;
@@ -79,6 +79,7 @@ public class DataRepair {
 						edata.errorFlagSet.set(validator.getIndex());
 						errorTable.put(tuple.getValue(rawAttrs.RUID), edata);
 					}
+					tuple.setValue(validator.getColName(), " ");
 					/*
 					System.out.println(tuple.getValue(rawAttrs.RUID_INDEX) + "\t" +
 							validator.getColName() + "\t" + 
@@ -151,7 +152,7 @@ public class DataRepair {
 			HashMap<String, Integer> tmp = new HashMap<String, Integer>();
 			for (Tuple tuple : list) {
 				String v = tuple.getValue(i);
-				if (v == null) v += " ";
+				if (v == null) v += "  ";
 				if (tmp.get(v) == null) {
 					tmp.put(v, 1);
 				} else {
@@ -162,13 +163,13 @@ public class DataRepair {
 			Set<String> keys = tmp.keySet();
 			int max = 0;
 			String correct = "";
-			for(String key: keys){  
-	            if (tmp.get(key) >= max) {
+			for(String key: keys){
+	            if (!key.equals(" ") && tmp.get(key) >= max) {
 	            	max = tmp.get(key);
 	            	correct = key;
 	            }
 	        }
-			if (correct == " ") correct = "";
+			if (correct.equals("  ")) correct = "";
 			for (Tuple tuple : list) {
 				String v = tuple.getValue(i);
 				if (!v.equals(correct)) {
