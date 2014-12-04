dfca50ac026ce130b9e9c70a39b2d809341bb5aa
jin-yc10 <ccjinyc@gmail.com>
2014-12-04 11:43:22 +0800

    Add apmt validate

--- DQCupJinYC/src/dqcup/repair/validators/APMTValidator.java
+++ DQCupJinYC/src/dqcup/repair/validators/APMTValidator.java
@@ -19,7 +19,19 @@ public class APMTValidator implements Validator {
 	@Override
 	public boolean test(String value) {
 		// TODO Auto-generated method stub
-		return true;
+		if(value == null || value.equals(""))
+			return true;
+		else if( value.length() != 3) {
+			return false;
+		}
+		else {
+			char c1 = value.charAt(0);
+			char c2 = value.charAt(1);
+			char c3 = value.charAt(2);
+			if( Character.isDigit(c1) && Character.isLowerCase(c2) && Character.isDigit(c3)) {
+				return true;
+			}
+			return false;
+		}
 	}
-
 }
